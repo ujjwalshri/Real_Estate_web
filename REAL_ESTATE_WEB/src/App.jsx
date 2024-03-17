ffe03067d24bcb640410c6495ec12312aac89f1f
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2024-03-18 00:01:59 +0530

    added

--- REAL_ESTATE_WEB/src/App.jsx
+++ REAL_ESTATE_WEB/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react';
+import './index.css';
+import NavBar from './components/NavBar';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <NavBar/>
+    <div className="container mx-auto mt-10">
+      <h1 className="text-4xl font-bold text-blue-500 mb-4">Hello, Tailwind CSS!</h1>
+      <p className="text-lg text-gray-700">This is a React app using Tailwind CSS.</p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Click me</button>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- REAL_ESTATE_WEB/src/components/NavBar.jsx
+++ REAL_ESTATE_WEB/src/components/NavBar.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white font-bold">REAL_STATE_DUNIA</div>
+        <ul className="flex space-x-4">
+          <li><a href="#" className="text-white hover:text-gray-300">Home</a></li>
+          <li><a href="#" className="text-white hover:text-gray-300">About</a></li>
+          <li><a href="#" className="text-white hover:text-gray-300">Services</a></li>
+          <li><a href="#" className="text-white hover:text-gray-300">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+    </>
   )
   }
 
